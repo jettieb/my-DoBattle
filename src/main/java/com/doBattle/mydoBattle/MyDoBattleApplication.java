19564a58d04189d8c5c3378e07309d358d8c18c2
Bokjihee <bokjihee@naver.com>
2024-03-01 19:06:33 +0900

    chore: initial commit

---
+++ src/main/java/com/doBattle/mydoBattle/MyDoBattleApplication.java
@@ -0,0 +1,13 @@
+package com.doBattle.mydoBattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDoBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDoBattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doBattle/mydoBattle/MyDoBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doBattle.mydoBattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDoBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
