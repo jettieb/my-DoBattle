a3781b66cf33412a77f2974393619da5ff729422
Bokjihee <bokjihee@naver.com>
2024-03-07 15:27:01 +0900

    feat: 완료한 투두 먼저 보이도록

--- src/main/java/com/doBattle/mydoBattle/repository/TodoRepository.java
+++ src/main/java/com/doBattle/mydoBattle/repository/TodoRepository.java
@@ -8,11 +8,13 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-    @Query(value = "SELECT * FROM todo WHERE battle_code = :battleCode AND member_id = :memberId AND create_date = :date", nativeQuery = true)
-    //참여자의 해당 배틀의 날짜에 따른 투두 찾기
+    @Query(value = "SELECT * FROM todo " +
+            "WHERE battle_code = :battleCode AND member_id = :memberId AND create_date = :date " +
+            "ORDER BY click DESC", nativeQuery = true)
+    //참여자의 해당 배틀의 날짜에 따른 투두 찾기 (완료한 투두 먼저 보이도록)
     List<Todo> findByBattleCodeAndMemberIdAndDate(Long battleCode, Long memberId, LocalDate date);
 
     @Query(value = "SELECT * FROM todo WHERE battle_code = :battleCode AND member_id = :memberId AND create_date = :date AND click = true", nativeQuery = true)
-    //참여자의 해당 배틀의 날짜에 따른 투두 찾기
+    //참여자의 해당 배틀의 날짜에 따른 투두 찾기(True만)
     List<Todo> findByBattleCodeAndMemberIdAndDateWithTrueValue(Long battleCode, Long memberId, LocalDate date);
 }
